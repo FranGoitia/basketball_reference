b04d6d07ea740b90f43a976e9e3b79eec50240fb
Fran Goitia <frangoitia@gmail.com>
2017-04-02 22:07:03 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='basketball_reference',
+    description='basketball reference scraper',
+    keywords=['basketball reference', 'scraper'],
+    version='1.0',
+    author='Francisco Goitia',
+    author_email='frangoitia@gmail.com',
+    url='https://github.com/FranGoitia/basketball_reference',
+    license='LICENSE.txt',
+    install_requires=['python-levenshtein', 'bs4', 'requests', 'wikipedia'],
+    scripts=['match_generator']
+)
