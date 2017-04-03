3bb5db6047a0364712633541aac30a12fd23e3d5
Fran Goitia <frangoitia@gmail.com>
2017-04-02 21:51:27 -0300

    create main method in match generator

--- match_generator.py
+++ match_generator.py
@@ -11,18 +11,21 @@ with open('logging.json', 'r') as f:
 logger = logging.getLogger('stringer-bell')
 
 
+def main(league, seasons):
+    seasons = get_seasons(seasons)
+    for season in seasons:
+        path = './matches/{0}/{1}/{2}'.format('united_states', 'nba', season)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        logger.info('Crawling season {0}'.format(season))
+        b_ref = NbaBRefSeason('united_states', league, season)
+        b_ref.crawl_season()
+
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--league', default='nba')
     parser.add_argument('--seasons', nargs='+', default=['2014-2015'])
     parser.add_argument('--date', default='10')
     args = parser.parse_args()
-
-    seasons = get_seasons(args.seasons)
-    for season in seasons:
-        path = './matches/{0}/{1}/{2}'.format('united_states', 'nba', season)
-        if not os.path.exists(path):
-            os.makedirs(path)
-        logger.info('Crawling season {0}'.format(season))
-        b_ref = NbaBRefSeason('united_states', args.league, season)
-        b_ref.crawl_season()
+    main(args.league, args.seasons)
